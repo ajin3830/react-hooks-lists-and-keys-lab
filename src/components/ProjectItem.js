1eaf1918395e55eb738369e72509a5894125cdea
ajin3830 <ajin3830@gmail.com>
2023-02-02 20:38:29 -0500

    completed lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+// every item is an object
+// props object in the parameter of this function has been destructured
+
+
+  const techArr = technologies.map(tech => {
+    return <span key={tech}>{tech}</span> 
+  });
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techArr}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,31 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectListElement = projects.map(project => {
+    return (
+      // console.log(project)
+      <ProjectItem 
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}/>
+    );
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectListElement}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+// projects is an array of objects
+// projects are props
+// props are project items
+// each project item displays inside project list element tag 
+// each with its own key
